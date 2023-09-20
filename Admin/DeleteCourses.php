b5713d5408b2ecdf1b3b48cdb156d6fd73a7df8b
Kareem Abdel Nabi <116344832+karreemm@users.noreply.github.com>
2023-09-21 02:46:25 +0300

    Update DeleteCourses.php
    12

--- Admin/DeleteCourses.php
+++ Admin/DeleteCourses.php
@@ -1,24 +1,35 @@
 <?php
 
 include '../Components/db.php';
-$course_id=$_GET["cid"];
+$course_id = $_GET["cid"];
 $flag = $_GET['flag'];
 
-if($flag=='workshop'){
+if ($flag == 'workshop') {
+  $sql1 = "DELETE FROM courses WHERE c_id=$course_id";
 
-  $sql1="DELETE FROM courses WHERE c_id=$course_id";
-  
-  if($conn->query($sql1)==true){ echo"
-<div class='alert alert-success' role='alert'>
-  Your Workshop Has Been Deleted Successfully!
-</div>
-'; header('location: Admin/Dashboard.php'); }
- else { echo'Error: $sql <br />
-$conn->error '; } } else if($flag=='diploma'){ $sql2='DELETE FROM diplomas WHERE
-c_id=$course_id'; if($conn->query($sql2)==true){ echo'
-<div class='alert alert-success' role='alert'>
-  Your Workshop Has Been Deleted Successfully!
-</div>
-";
- header("location: ../Admin/Dashboard.php"); } else { echo"Error: $sql <br />
-$conn->error "; } } ?>
+  if ($conn->query($sql1) === TRUE) {
+    echo '
+    <div class="alert alert-success" role="alert">
+      Your Workshop Has Been Deleted Successfully!
+    </div>';
+    header('location: Admin/Dashboard.php');
+    exit;
+  } else {
+    echo 'Error: ' . $sql1 . '<br />' . $conn->error;
+  }
+} elseif ($flag == 'diploma') {
+  $sql2 = "DELETE FROM diplomas WHERE c_id=$course_id";
+
+  if ($conn->query($sql2) === TRUE) {
+    echo '
+    <div class="alert alert-success" role="alert">
+      Your Diploma Has Been Deleted Successfully!
+    </div>';
+    header("location: ../Admin/Dashboard.php");
+    exit;
+  } else {
+    echo 'Error: ' . $sql2 . '<br />' . $conn->error;
+  }
+}
+
+?>
